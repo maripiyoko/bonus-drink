7133e7b8b334a7dc3c7b359c0af208895c5c04c7
Mariko <maripiyok@gmail.com>
2015-06-14 18:10:18 +0900

    おまけの個数を再帰でカウントする関数を追加。 最初に買った本数と計算したおまけを合わせて、全体で取得出来る本数を計算する。

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,11 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    amount + omake_count_for(amount)
+  end
+
+  def self.omake_count_for(amount)
+    omake, nokori = amount.divmod(3)
+    consider = omake + nokori
+    consider > 2 ? omake + omake_count_for(consider) : omake
   end
 end
